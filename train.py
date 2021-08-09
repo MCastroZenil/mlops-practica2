1807cf1427a3ce114bdc2d2b6e87f5ada3dd265c
MCastroZenil <88677556+MCastroZenil@users.noreply.github.com>
2021-08-09 14:47:59 -0500

    Update train.py

--- train.py
+++ train.py
@@ -47,6 +47,7 @@ df['pred_accuracy'] = score_int
 
 # Bar plot by region
 
+
 sns.set_color_codes("dark")
 ax = sns.barplot(x="region", y="pred_accuracy", data=df, palette = "Greens_d")
 ax.set(xlabel="Region", ylabel = "Model accuracy")
